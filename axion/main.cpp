c0f329a38898eb7530b334890137b82bc8773cc4
Ethan Coeytaux <ethanaco@protonmail.com>
2023-03-06 20:05:07 -0700

    Initial Commit

---
+++ axion/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  axion
+//
+//  Created by Ethan Coeytaux on 3/6/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
